aba4679be61b8960d369a63ad79d8067a2dd2ca5
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-06-10 00:44:37 +0800

    Add a method

--- spec/google.rb
+++ spec/google.rb
@@ -6,8 +6,13 @@ require './spec/shared.rb'
 include Helper
 include Google
 
+def method_also_works
+  search 'Yes, it works!'
+end
+
 feature 'Google should work' do
   scenario 'Go to google' do
     search 'Trung'
+    method_also_works
   end
 end
